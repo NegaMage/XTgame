a66a051c5209f254977bd9f781ea0ca6681cca15
Feyaz Baker <feyazabubaker@gmail.com>
2021-03-16 22:21:47 +0530

    Automation part 1 done.
    Next, we need to automate the file writing in XTGame

--- analysis.py
+++ analysis.py
@@ -1,66 +1,82 @@
-types = [0,1,3,5]
+import os
+
+types = range(11)
 
 strings = ["file_"+str(i)+"_"+str(j) for i in types for j in types]
 
-print(strings)
+f = open("test", "w")
+f.write("1000 {} {}".format(1, 3))
+f.close()
 
-for name in strings:
-    print("Checking for "+name)
-    res = open("rawData/"+name)
-    res = res.read()
+os.system('./a.out < test')
 
-    # print(type(res))
-    res = res.split("\n")
-    # print(res)
-    lens = {
-        'x': 0,
-        'o': 0,
-        '*': 0
-    }
+for i in range(11):
+    for j in range(11):
+        f = open("test", "w")
+        f.write("1000 {} {}".format(i, j))
+        f.close()
 
-    wins = {
-        'x': 0,
-        'o': 0,
-        '*': 0
-    }
-    avg_lengths = {
-         'x': 0,
-         'o': 0,
-         '*': 0
-    }
+        os.system('./a.out < test')
 
-    x_count = 0
-    x_length = 0
-    o_count = 0
-    o_length = 0
-    d_count = 0
-    d_length = 0
+# print(strings)
 
-    for game in res:
-        if game: # game is not empty string
-            game = game.split(" ")
-            wins[game[-1][-1]]+=1
-            lens[game[-1][-1]]+=len(game)-1
-            if game[-1][-1] == 'x':
-                x_count += 1
-                x_length += len(game) - 1
-            elif game[-1][-1] == 'o':
-                o_count += 1
-                o_length += len(game) - 1
-            elif game[-1][-1] == '*':
-                d_count += 1
-                d_length += len(game) - 1
+# for name in strings:
+#     print("Checking for "+name)
+#     res = open("rawData/"+name)
+#     res = res.read()
 
-    avg_lengths['x'] = x_length / x_count
-    avg_lengths['o'] = o_length / o_count
-    avg_lengths['*'] = d_length / d_count
+#     # print(type(res))
+#     res = res.split("\n")
+#     # print(res)
+#     lens = {
+#         'x': 0,
+#         'o': 0,
+#         '*': 0
+#     }
+
+#     wins = {
+#         'x': 0,
+#         'o': 0,
+#         '*': 0
+#     }
+#     avg_lengths = {
+#          'x': 0,
+#          'o': 0,
+#          '*': 0
+#     }
+
+#     x_count = 0
+#     x_length = 0
+#     o_count = 0
+#     o_length = 0
+#     d_count = 0
+#     d_length = 0
+
+#     for game in res:
+#         if game: # game is not empty string
+#             game = game.split(" ")
+#             wins[game[-1][-1]]+=1
+#             lens[game[-1][-1]]+=len(game)-1
+#             if game[-1][-1] == 'x':
+#                 x_count += 1
+#                 x_length += len(game) - 1
+#             elif game[-1][-1] == 'o':
+#                 o_count += 1
+#                 o_length += len(game) - 1
+#             elif game[-1][-1] == '*':
+#                 d_count += 1
+#                 d_length += len(game) - 1
+
+#     avg_lengths['x'] = x_length / x_count
+#     avg_lengths['o'] = o_length / o_count
+#     avg_lengths['*'] = d_length / d_count
     
-    print("Number of wins:")
-    print(wins)
+#     print("Number of wins:")
+#     print(wins)
 
-    print("Number of steps taken:")
-    print(lens)
+#     print("Number of steps taken:")
+#     print(lens)
 
-    print(" Average game lengths to win:")
-    print(avg_lengths)
+#     print(" Average game lengths to win:")
+#     print(avg_lengths)
 
