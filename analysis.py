28d2f7fe1189f173e847ec73f850f04259dd8ac0
uamukh <___arunava.mukhoti@ushur.com>
2021-03-24 15:51:23 +0530

    table creation

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
 import os
+import pandas as pd
 
 # types = [0,1,3,5,7,8,9,10,11]
-types = range(12)
+types = [0,1,2,3,4,5,6,7]
 
 strings = [str(i)+"_"+str(j) for i in types for j in types]
 
@@ -11,6 +12,8 @@ strings = [str(i)+"_"+str(j) for i in types for j in types]
 
 # os.system('./a.out < test')
 
+
+
 for i in types:
     for j in types:
         f = open("test", "w")
@@ -19,7 +22,14 @@ for i in types:
 
         os.system('./a.out < test')
 
-# print(strings)
+print(strings)
+data_t1 = {}
+data_t2 = {}
+data_t3 = {}
+for i in types:
+    data_t1[str(i)] = []
+    data_t2[str(i)] = []
+    data_t3[str(i)] = []
 
 for name in strings:
     print("Checking for "+name)
@@ -28,12 +38,14 @@ for name in strings:
 
     # print(type(res))
     res = res.split("\n")
-    # print(res)
+    #print(res)
+    '''
     lens = {
         'x': 0,
         'o': 0,
         '*': 0
     }
+    '''
 
     wins = {
         'x': 0,
@@ -57,7 +69,7 @@ for name in strings:
         if game: # game is not empty string
             game = game.split(" ")
             wins[game[-1][-1]]+=1
-            lens[game[-1][-1]]+=len(game)-1
+            #lens[game[-1][-1]]+=len(game)-1
             if game[-1][-1] == 'x':
                 x_count += 1
                 x_length += len(game) - 1
@@ -75,9 +87,31 @@ for name in strings:
     print("Number of wins:")
     print(wins)
 
-    print("Number of steps taken:")
-    print(lens)
+    #print("Number of steps taken:")
+    #print(lens)
 
     print(" Average game lengths to win:")
     print(avg_lengths)
+    x_perc_win = round((wins['x']/(wins['x'] + wins['o'])) * 100.0 , 1)
+    o_perc_win = round((wins['o']/(wins['x'] + wins['o'])) * 100.0 , 1)
+    draw_perc = round((wins['*']/(wins['x'] + wins['o'] + wins['*'])) * 100.0,1)
 
+    cur_t1 = str(x_perc_win) + '% , ' + str(o_perc_win) + '%'
+    cur_t2 = str(round(avg_lengths['x'],1)) + ' ,' + str(round(avg_lengths['o'],1))
+    cur_t3 = str(draw_perc) + '% , ' + str(round(avg_lengths['*'],1))
+    
+    data_t1[name[2]].append(cur_t1)
+    data_t2[name[2]].append(cur_t2)
+    data_t3[name[2]].append(cur_t3)
+
+df_t1 = pd.DataFrame.from_dict(data_t1)
+df_t2 = pd.DataFrame.from_dict(data_t2)
+df_t3 = pd.DataFrame.from_dict(data_t3)
+df_t1.to_csv('df_t1.csv')
+df_t2.to_csv('df_t2.csv')
+df_t3.to_csv('df_t3.csv')
+print(df_t1.to_latex())
+print(df_t2.to_latex())
+print(df_t3.to_latex())
+
+    
